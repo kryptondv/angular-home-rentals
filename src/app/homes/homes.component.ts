f12a74311052459abf4745c4cb2066c829c230fe
kryptondv <kryptondevpl@gmail.com>
2020-12-05 19:11:36 +0100

    add data.service and render data in homes component

--- src/app/homes/homes.component.ts
+++ src/app/homes/homes.component.ts
@@ -1,15 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import { DataService } from '../data.service';
 
 @Component({
   selector: 'app-homes',
   templateUrl: './homes.component.html',
-  styleUrls: ['./homes.component.scss']
+  styleUrls: ['./homes.component.scss'],
 })
 export class HomesComponent implements OnInit {
+  homes$ = this.dataService.getHomes();
 
-  constructor() { }
+  constructor(private dataService: DataService) {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
