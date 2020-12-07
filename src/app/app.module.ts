d5c74239c6f610114fe5a8ffd359dc2c53a28d28
kryptondv <kryptondevpl@gmail.com>
2020-12-07 10:39:54 +0100

    add home-search component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,9 +7,10 @@ import { AppComponent } from './app.component';
 import { HomesComponent } from './homes/homes.component';
 import { HomeTypeFilterComponent } from './home-type-filter/home-type-filter.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HomeSearchComponent } from './home-search/home-search.component';
 
 @NgModule({
-  declarations: [AppComponent, HomesComponent, HomeTypeFilterComponent],
+  declarations: [AppComponent, HomesComponent, HomeTypeFilterComponent, HomeSearchComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
